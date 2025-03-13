f3a7bd7026f391b37b52b2c0fd11f25ca635c2d7
Viga Bots <133951124+vigarepo2@users.noreply.github.com>
2025-03-13 16:14:51 +0530

    Update worker.js

--- worker.js
+++ worker.js
@@ -3,9 +3,9 @@
 const BOT_TOKEN = "BOT_TOKEN"; // Insert your bot token.
 const BOT_WEBHOOK = "/endpoint"; // Let it be as it is.
 const BOT_SECRET = "BOT_SECRET"; // Insert a powerful secret text (only [A-Z, a-z, 0-9, _, -] are allowed).
-const BOT_OWNER = 123456789; // Insert your telegram account id.
-const BOT_CHANNEL = -100123456789; // Insert your telegram channel id which the bot is admin in.
-const SIA_SECRET = "SIA_SECRET"; // Insert a powerful secret text and keep it safe.
+const BOT_OWNER = 6986536422; // Insert your telegram account id.
+const BOT_CHANNEL = -1001872371917; // Insert your telegram channel id which the bot is admin in.
+const SIA_SECRET = "K9$pR#2tZx@7!mQw5*LnY8vBs3^dE6cG"; // Insert a powerful secret text and keep it safe.
 const PUBLIC_BOT = false; // Make your bot public (only [true, false] are allowed).
 
 // ---------- Do Not Modify ---------- // 
